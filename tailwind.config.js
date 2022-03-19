e36e415000f22049f8792d8e9dd0b9737a363939
Aminur Islam <aminur.revo@gmail.com>
2022-03-20 00:04:55 +0600

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    screens: {
+      'xl': '1147px',
+      '2xl': '1147px',
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif'],
+    },
+    colors: {
+      'giopio-orange': '#FB6400',
+      'giopio-black': '#26404C',
+      'giopio-text': '#4A5871',
+    }
+  },
+  plugins: [],
+}
