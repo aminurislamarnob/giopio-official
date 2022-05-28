66a0e43c8fea9bceb90b5d847b428e86d3c7171a
Aminur Islam <aminur.revo@gmail.com>
2022-05-28 23:18:38 +0600

    project-details page done & all pages responsive for small screen laptop

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,29 @@ module.exports = {
       center: true,
       padding: '1rem',
     },
-    // screens: {
-    //   'xl': '1147px',
-    //   '2xl': '1147px',
-    // },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1700px',
+      // => @media (min-width: 1700px) { ... }
+    },
     fontFamily: {
       'montserrat': ['Montserrat', 'sans-serif'],
     },
     colors: {
       transparent: 'transparent',
       'giopio-orange': '#FB6400',
+      'giopio-light-orange': '#FFF3F3',
       'giopio-black': '#26404C',
       'giopio-text': '#4A5871',
       'white': '#ffffff',
